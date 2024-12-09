afe2f4a1e5fdce26be27dac9ee232dbb32ec8e6a
Sergey Morozov <morozov.serg901@gmail.com>
2024-12-09 21:09:04 +0300

    Implement sliding window in linearForward transform. Average bandwidth 23 mb/s.

--- lib/LSX.cpp
+++ lib/LSX.cpp
@@ -1,6 +1,7 @@
 #include "LSX.h"
 #include "GF.h"
 #include "Types.h"
+#include <algorithm>
 
 Block LSXForward::operator()(Block key, Block input) {
   auto block = xorForward(key, input);
@@ -12,32 +13,35 @@ Block LSXForward::linearForward(int rounds, Block input) {
   GF<0b111000011> gf;
 
   auto bytes = toByteArray(input);
+  uint8_t data[32];
+
+  std::copy(bytes.data, bytes.data + 16, data);
+  int pos = 16;
+
   while (rounds--) {
     uint8_t transformed = 0;
 
-    transformed = gf.add(transformed, gf.multiply(1, bytes.data[0]));
-    transformed = gf.add(transformed, gf.multiply(148, bytes.data[1]));
-    transformed = gf.add(transformed, gf.multiply(32, bytes.data[2]));
-    transformed = gf.add(transformed, gf.multiply(133, bytes.data[3]));
-    transformed = gf.add(transformed, gf.multiply(16, bytes.data[4]));
-    transformed = gf.add(transformed, gf.multiply(194, bytes.data[5]));
-    transformed = gf.add(transformed, gf.multiply(192, bytes.data[6]));
-    transformed = gf.add(transformed, gf.multiply(1, bytes.data[7]));
-    transformed = gf.add(transformed, gf.multiply(251, bytes.data[8]));
-    transformed = gf.add(transformed, gf.multiply(1, bytes.data[9]));
-    transformed = gf.add(transformed, gf.multiply(192, bytes.data[10]));
-    transformed = gf.add(transformed, gf.multiply(194, bytes.data[11]));
-    transformed = gf.add(transformed, gf.multiply(16, bytes.data[12]));
-    transformed = gf.add(transformed, gf.multiply(133, bytes.data[13]));
-    transformed = gf.add(transformed, gf.multiply(32, bytes.data[14]));
-    transformed = gf.add(transformed, gf.multiply(148, bytes.data[15]));
+    transformed = gf.add(transformed, gf.multiply(1, data[pos - 16 + 0]));
+    transformed = gf.add(transformed, gf.multiply(148, data[pos - 16 + 1]));
+    transformed = gf.add(transformed, gf.multiply(32, data[pos - 16 + 2]));
+    transformed = gf.add(transformed, gf.multiply(133, data[pos - 16 + 3]));
+    transformed = gf.add(transformed, gf.multiply(16, data[pos - 16 + 4]));
+    transformed = gf.add(transformed, gf.multiply(194, data[pos - 16 + 5]));
+    transformed = gf.add(transformed, gf.multiply(192, data[pos - 16 + 6]));
+    transformed = gf.add(transformed, gf.multiply(1, data[pos - 16 + 7]));
+    transformed = gf.add(transformed, gf.multiply(251, data[pos - 16 + 8]));
+    transformed = gf.add(transformed, gf.multiply(1, data[pos - 16 + 9]));
+    transformed = gf.add(transformed, gf.multiply(192, data[pos - 16 + 10]));
+    transformed = gf.add(transformed, gf.multiply(194, data[pos - 16 + 11]));
+    transformed = gf.add(transformed, gf.multiply(16, data[pos - 16 + 12]));
+    transformed = gf.add(transformed, gf.multiply(133, data[pos - 16 + 13]));
+    transformed = gf.add(transformed, gf.multiply(32, data[pos - 16 + 14]));
+    transformed = gf.add(transformed, gf.multiply(148, data[pos - 16 + 15]));
 
-    for (int i = 1; i < 16; ++i) {
-      std::swap(bytes.data[i], bytes.data[i - 1]);
-    }
-    bytes.data[15] = transformed;
+    data[pos++] = transformed;
   }
 
+  std::copy(data + (pos - 16), data + pos, bytes.data);
   return fromByteArray(std::move(bytes));
 }
 
