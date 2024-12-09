303a7702fd331ea3199f407428fae52227f7335d
Sergey Morozov <morozov.serg901@gmail.com>
2024-12-10 01:40:37 +0300

    Reduce cache utilization by placing output in the end. Average bandwidth: 114-119 mb/s.

--- main.cpp
+++ main.cpp
@@ -13,17 +13,15 @@ int main() {
   std::chrono::nanoseconds estimated_time = std::chrono::nanoseconds::zero();
 
   Block block = 0;
-  while (true) {
+  while (estimated_time.count() < 5e9) {
     estimated_time += measure<std::chrono::nanoseconds>::duration(
         &Encryptor::encrypt, &encryptor, block);
     processed += sizeof(block);
     block++;
-
-    if (block % 10 == 0) {
-      std::cout << "Encryption bandwidth is about "
-                << static_cast<double>(processed) / (1 << 20) /
-                       (estimated_time.count() / 1e9)
-                << " megabytes/sec" << std::endl;
-    }
   }
+
+  std::cout << "Encryption bandwidth is about "
+            << static_cast<double>(processed) / (1 << 20) /
+                   (estimated_time.count() / 1e9)
+            << " megabytes/sec" << std::endl;
 }
